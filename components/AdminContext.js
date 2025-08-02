ee9419b5a4d2a9c3928df59b4e5f982391e8ed57
Timo Kiukkanen <timo@kiukkanen.com>
2025-08-02 14:59:31 +0100

    website changes

--- components/AdminContext.js
+++ components/AdminContext.js
@@ -2,24 +2,28 @@ import React, { createContext, useContext, useState } from 'react';
 
 const AdminContext = createContext();
 
-export const useAdmin = () => useContext(AdminContext);
-
-export const AdminProvider = ({ children }) => {
+export function AdminProvider({ children }) {
   const [isAdmin, setIsAdmin] = useState(false);
 
   const loginAsAdmin = (password) => {
-    if (password === 'supersecret') {
+    if (password === process.env.NEXT_PUBLIC_ADMIN_PASSWORD) {
       setIsAdmin(true);
       return true;
     }
     return false;
   };
 
-  const logout = () => setIsAdmin(false);
+  const logout = () => {
+    setIsAdmin(false);
+  };
 
   return (
     <AdminContext.Provider value={{ isAdmin, loginAsAdmin, logout }}>
       {children}
     </AdminContext.Provider>
   );
-};
+}
+
+export function useAdmin() {
+  return useContext(AdminContext);
+}
--- pages/_app.js.js
+++ pages/_app.js.js
@@ -1,10 +1,10 @@
 // pages/_app.js
-import '../styles/main.css';
-import { AdminProvider } from '../components/AdminContext';  // Import your provider
+import '../styles/globals.css';
+import { AdminProvider } from '../components/AdminContext'; // adjust path if needed
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AdminProvider>     {/* Wrap whole app here */}
+    <AdminProvider>
       <Component {...pageProps} />
     </AdminProvider>
   );
