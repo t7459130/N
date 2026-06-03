668e1fa111d3bb10a6b4106991ff990c393f493a
Rehan Dar <rehandar@windsorgreatcars.com>
2026-06-03 21:04:02 +0100

    doing chatgpts work for them

--- components/Layout.js
+++ components/Layout.js
@@ -4,9 +4,18 @@ import { FaBars, FaTimes, FaPhone } from 'react-icons/fa';
 
 export default function Layout({ children }) {
   const [open, setOpen] = useState(false);
+  const [mobileLogoIndex, setMobileLogoIndex] = useState(0);
+
   const menuRef = useRef(null);
 
-  const logos = [
+  const desktopLogos = [
+    '/images/ferrari.png',
+    '/images/lamborghini.png',
+    '/images/rolls.png',
+    '/images/bentley.png',
+  ];
+
+  const mobileLogos = [
     '/images/ferrari.png',
     '/images/lamborghini.png',
     '/images/rolls.png',
@@ -14,7 +23,7 @@ export default function Layout({ children }) {
     '/images/aston.png',
     '/images/bugatti.png',
     '/images/mercedes.png',
-    '/images/porsche.png'
+    '/images/porsche.png',
   ];
 
   useEffect(() => {
@@ -23,8 +32,20 @@ export default function Layout({ children }) {
         setOpen(false);
       }
     };
+
     document.addEventListener('mousedown', handler);
-    return () => document.removeEventListener('mousedown', handler);
+
+    return () => {
+      document.removeEventListener('mousedown', handler);
+    };
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMobileLogoIndex((prev) => (prev + 1) % mobileLogos.length);
+    }, 2500);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -32,49 +53,4 @@ export default function Layout({ children }) {
 
       <header className="site-header">
 
-        {/* LEFT */}
-        <div className="nav-left">
-          <a href="tel:1234567890" className="phone">
-            <FaPhone />
-          </a>
-
-          <Link href="/">Home</Link>
-          <Link href="/Inventory">Stock</Link>
-          <Link href="/Sellyourcar">Sell</Link>
-        </div>
-
-        {/* CENTER LOGOS */}
-        <div className="logo-box">
-          {logos.slice(0, 4).map((l, i) => (
-            <img key={i} src={l} alt="" />
-          ))}
-        </div>
-
-        {/* RIGHT */}
-        <div className="nav-right">
-          <Link href="/NewsAndEvents">Insights</Link>
-          <Link href="/About">About</Link>
-          <Link href="/contact">Contact</Link>
-
-          <button className="icon-btn" onClick={() => setOpen(!open)}>
-            {open ? <FaTimes /> : <FaBars />}
-          </button>
-        </div>
-
-        {/* MOBILE MENU */}
-        <nav ref={menuRef} className={`mobile-menu ${open ? 'open' : ''}`}>
-          <Link href="/">Home</Link>
-          <Link href="/Inventory">Stock</Link>
-          <Link href="/Sellyourcar">Sell</Link>
-          <Link href="/NewsAndEvents">Insights</Link>
-          <Link href="/About">About</Link>
-          <Link href="/contact">Contact</Link>
-        </nav>
-
-      </header>
-
-      <main className="page">{children}</main>
-
-    </div>
-  );
-}
+        {/* LEFT NAV
